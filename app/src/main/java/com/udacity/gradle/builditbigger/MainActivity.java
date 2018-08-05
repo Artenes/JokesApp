d4e5b6f860529f21a2c6217c76d8d3b7f5e55571
Artenes Nogueira <artenesama@gmail.com>
2018-08-05 10:26:46 -0400

    I would avoid the sushi if I was you. It’s a little fishy.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -3,8 +3,6 @@ package com.udacity.gradle.builditbigger;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
